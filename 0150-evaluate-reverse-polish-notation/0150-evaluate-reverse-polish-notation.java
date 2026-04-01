d60a0db4b5bd3fa1e1d73979a169fa9dc06a0a8d
EESHIKA MUKHOPADHYAY <dazzlingphoenix2022@gmail.com>
2026-04-01 22:20:47 +0530

    Time: 8 ms (12.43%), Space: 47.2 MB (5.08%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<String> stk = new Stack<>();
+
+        for(int i = 0 ; i < tokens.length ; i++){
+            String s = tokens[i];
+            char ch = s.charAt(s.length() - 1);
+            // boolean pop = false ;
+
+            if(ch == '+' || ch == '-' || ch=='*' || ch=='/'){
+                if( !stk.isEmpty() ){
+                    // pop=true;
+                    int n2 = Integer.valueOf(stk.pop());
+                    if(!stk.isEmpty()){
+                        int n1 =  Integer.valueOf(stk.pop()) ;
+                        if(ch == '+'){
+                            String op = Integer.toString(n1+n2); 
+                            stk.push(op);
+                        }else if(ch == '-'){
+                            String op = Integer.toString(n1-n2); 
+                            stk.push(op);
+                        }else if(ch == '*'){
+                            String op = Integer.toString(n1*n2); 
+                            stk.push(op);
+                        }else{
+                            String op = Integer.toString(n1/n2); 
+                            stk.push(op);
+                        }
+                    } 
+                }
+            }else{
+                stk.push(s);
+            }
+
+        }
+
+        return Integer.valueOf(stk.pop());
+    }
+}
