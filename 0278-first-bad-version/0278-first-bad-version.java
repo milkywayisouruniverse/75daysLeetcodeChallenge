e575dad21fcb772f2b2aa53ed96d1e471b7b12f4
EESHIKA MUKHOPADHYAY <dazzlingphoenix2022@gmail.com>
2026-05-12 22:09:03 +0530

    Time: 15 ms (13.35%), Space: 41.8 MB (84.86%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,15 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int s = 1;
+        int e = n;
+        while(s <= e) {
+            int m = s + ((e - s) >> 1);
+            if(isBadVersion(m)) {
+                e = m - 1;
+            }else {
+                s = m + 1;
+            }
+        }
+        return s;
+    }
+}
